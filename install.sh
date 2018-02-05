bfb28c1db4a532fb22fc20967ad270b017c4e212
olivia <olivia@fastmail.com>
2018-02-05 14:38:36 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/data-mining-code/compose
+git clone https://github.com/data-mining-code/frontend
+git clone https://github.com/data-mining-code/broker
+git clone https://github.com/data-mining-code/pythontest
+
+docker-compose up -d
