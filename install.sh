ec3ad595dff1fa47638bf21e4d0ae7736d026fd7
olivia <olivia@fastmail.com>
2018-02-05 14:40:30 +0100

    remove compose clone

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-git clone https://github.com/data-mining-code/compose ../compose
 git clone https://github.com/data-mining-code/frontend ../frontend
 git clone https://github.com/data-mining-code/broker ../broker
 git clone https://github.com/data-mining-code/pythontest ../pythontest
