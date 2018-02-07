88b58ded7f37aa3550fdc5ccf824c303ce97928e
olivia <olivia@fastmail.com>
2018-02-07 15:09:19 +0100

    add env configuration

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ git clone https://github.com/data-mining-code/frontend ../frontend
 git clone https://github.com/data-mining-code/broker ../broker
 git clone https://github.com/data-mining-code/pythontest ../pythontest
 
+cp .env.example .env
 docker-compose up -d
